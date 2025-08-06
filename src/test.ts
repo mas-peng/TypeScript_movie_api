da6c53a34b52fce11a94e72893398e0d6d883675
pr-haru <harukikose2021@gmail.com>
2025-08-06 19:44:02 +0900

    コミット

--- src/test.ts
+++ src/test.ts
@@ -1,1 +1,21 @@
-console.log('test')
+import { APIGatewayProxyEventV2, APIGatewayProxyResultV2 } from 'aws-lambda';
+
+/**
+ * @param event API Gatewayから渡されるイベントオブジェクト
+ * @returns API Gatewayが解釈できる形式のレスポンス
+ */
+export const handler = async (event: APIGatewayProxyEventV2): Promise<APIGatewayProxyResultV2> => {
+  
+  const fixedData = {
+    message: 'Hello from Lambda!',
+    status: 'ok',
+  };
+
+  return {
+    statusCode: 200,
+    headers: {
+      'Content-Type': 'application/json',
+    },
+    body: JSON.stringify(fixedData),
+  };
+};
