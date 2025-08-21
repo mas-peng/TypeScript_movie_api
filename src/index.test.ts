bd4dcd11a65c09231906f316e62bdc8826157378
pr-haru <harukikose2021@gmail.com>
2025-08-21 15:20:35 +0900

    異常系テストコード

--- src/index.test.ts
+++ src/index.test.ts
@@ -16,19 +16,41 @@ beforeEach(() => {
 // --- テストシナリオ ---
 
 // 1. 正常系: 項目が正常に取得できる場合のテスト
-test('IDが見つかった場合に項目データを返す', async () => {
-    // 準備: モックの戻り値を設定
-    const mockItem = { id: 1, name: 'test' };
-    ddbMock.on(GetCommand).resolves({ Item: mockItem });
+describe("項目が取得できる場合", () => {
+    test('IDが見つかった場合に項目データを返す', async () => {
+        // 準備: モックの戻り値を設定
+        const mockItem = { id: 1, name: 'test' };
+        // mockでGetCommandが実行された場合に、返却される値をresolvesの引数に設定
+        ddbMock.on(GetCommand).resolves({ Item: mockItem });
 
-    // 実行: モックされたDynamoDBクライアントが使われる
-    const result = await handler({
-        queryStringParameters: { id: '1' },
-    } as any);
+        // 実行: モックされたDynamoDBクライアントが使われる
+        const result = await handler({
+            queryStringParameters: { id: '1' },
+        } as any);
 
-    // 検証
-    expect(result.statusCode).toBe(200);
-    expect(result.body).toBe(JSON.stringify(mockItem));
+        // 検証
+        expect(result.statusCode).toBe(200);
+        expect(result.body).toBe(JSON.stringify(mockItem));
 
+    });
 });
 
+// 2. 異常系: 項目が取得できない場合のテスト
+describe("項目が取得できない場合", () => {
+    test('IDが見つからなかった場合に返す処理', async () => {
+        // 準備: モックの戻り値を設定
+        // 返却される値　resolvesには空を入れる
+        ddbMock.on(GetCommand).resolves({});
+
+        // 実行: モックされたDynamoDBクライアントが使われる
+        const resultError = await handler({
+            queryStringParameters: { id: '' },
+        } as any);
+
+        // 検証
+        expect(resultError.statusCode).toBe(404);
+        // 
+        expect(JSON.parse(resultError.body)).toEqual({ "message": "Item not found" });
+
+    });
+});
--- src/index.ts
+++ src/index.ts
@@ -9,14 +9,23 @@ const dynamodbClient = new DynamoDBClient({
 // ここで、↑で初期化したDynamoDBClientを用いてDynamoDBDocumentClientを初期化
 const docClient = DynamoDBDocumentClient.from(dynamodbClient);
 export const handler = async (event: APIGatewayProxyEvent): Promise<APIGatewayProxyResult> => {
+    const id = event.queryStringParameters?.id;
     const command = new GetCommand({
         TableName: "Api_Sample_Tbl",
         Key: {
-            id: 1,
+            id: id,
         },
       });
 const response = await docClient.send(command);
 const item = response.Item
+if(!item){
+      console.log(response);
+  return  {
+    statusCode: 404,
+    body: JSON.stringify({ message: 'Item not found' }),
+
+}
+}
   console.log(response);
   return  {
     statusCode: 200,
